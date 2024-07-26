3daa42e844e440cbf799967172d0d455e1d930ce
yujeong <parkyj0430@gmail.com>
2024-07-26 20:10:16 +0900

    Feat : 반려동물 선택하기 페이지 형식 구현

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Memory from './pages/Memory';
 import RegisterPet from './pages/RegisterPet';
+import ChoosePet from './pages/ChoosePet';
+import DiaryList from './pages/DiaryList';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path='/memory' element={<Memory/>}/>
       <Route path='/register-pet' element={<RegisterPet/>}/>
+      <Route path='/diary' element={<ChoosePet/>}/>
+      <Route path="/diary/pet:petId" element={<DiaryList />} />
     </Routes>
   );
 }
