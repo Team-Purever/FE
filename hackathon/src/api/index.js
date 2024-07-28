d09207ed2149cc174c6b34eb00a40a88ea8ac104
yujeong <parkyj0430@gmail.com>
2024-07-28 22:26:00 +0900

    Feat : axios instance/interceptor 설정

---
+++ hackathon/src/api/index.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+    baseURL: "YOUR_BASE_URL_HERE",
+    headers: {
+      'Content-Type': 'application/json',
+    }
+});
+
+// 토큰 재발급 함수
+const reissueToken = async () => {
+    const refreshToken = localStorage.getItem('refresh_token');
+    if (!refreshToken) {
+      console.error('리프레시 토큰이 없습니다');
+      return null;
+    }
+  
+    try {
+      const response = await axiosInstance.get('/auth/reissue', {
+        headers: {
+            refreshToken: `Bearer ${refreshToken}`
+        }
+      });
+      console.log('토큰 재발급 완료');
+      const newAccessToken = response.data.accessToken;
+      localStorage.setItem('access_token', newAccessToken);
+      return newAccessToken;
+
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+};
+
+// Axios 요청 인터셉터 설정 (accessToken 자동 설정)
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const accessToken = localStorage.getItem('access_token');
+        if (accessToken) {
+            config.headers['Authorization'] = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+// Axios 응답 인터셉터 설정
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      const originalRequest = error.config;
+  
+      // 401 응답일 때
+      if (error.response && error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+        const newAccessToken = await reissueToken(); // 토큰 재발급 시도
+        if (newAccessToken) {
+          originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+          return axiosInstance(originalRequest); // 재발급된 토큰으로 원래 요청 재시도
+        }
+      }
+  
+      return Promise.reject(error); // 에러 반환
+    }
+  );
